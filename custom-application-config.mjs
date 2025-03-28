cfa7b5041de155dbb0bc38bfe8d56bf639fd3f56
Behnam Tehrani <15884572+behnamt@users.noreply.github.com>
2025-03-28 11:06:42 -0400

    Update custom-application-config.mjs

--- custom-application-config.mjs
+++ custom-application-config.mjs
@@ -5,7 +5,7 @@ import { PERMISSIONS, entryPointUriPath } from './src/constants';
  */
 const config = {
   name: 'Shop Assist',
-  entryPointUriPath,
+  entryPointUriPath: '${env:ENTRY_POINT_URI_PATH}',
   cloudIdentifier: '${env:CLOUD_IDENTIFIER}',
   env: {
     development: {
